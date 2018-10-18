af09eb26f61d4a307c43f8bd9518d7ed7e9c6f22
Gustavo <gcallejas@grupoeducare.com>
2018-10-18 17:20:57 -0500

    Cambio rama desarrollo

--- modelos/usuarios.modelo.js
+++ modelos/usuarios.modelo.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose');
 // El objeto de tipo Esquema nos permite guardar en una colección concreta y en un documento concreto dentro de esa colección
 var Schema = mongoose.Schema;
 // Plugin para validar unico corrico
-//var uniqueValidator = require('mongoose-unique-validator');
+var uniqueValidator = require('mongoose-unique-validator');
 
 var rolesValidos = {
 	values:['ADMIN_ROLE','TEACHER_ROLE',"STUDENT_ROLE"],
@@ -16,7 +16,7 @@ var rolesValidos = {
 var UsuariosSchema = Schema({
 	
 	name: { type:String, required:[true,'El nombre es necesario'] },
-	email: { type:String,  required:[true,'El correo es necesario'] },
+	email: { type:String, index: true, unique: true, required:[true,'El correo es necesario'] },
 	password:{ type:String, required:[true,'El contraseña es necesaria'] },
 	imagen: { type:String, required:false },
 	role: { type:String, required:false, default:"TEACHER_ROLE", enum: rolesValidos },
@@ -24,7 +24,7 @@ var UsuariosSchema = Schema({
 	google : { type: Boolean , default:false}
 });
 
-//UsuariosSchema.plugin(uniqueValidator, { message: '{PATH} debe de ser único' });
+UsuariosSchema.plugin(uniqueValidator, { message: '{PATH} debe de ser único' });
 
 // El objeto Usuarios va poder ser instanciado y automáticamente le vamos asignando los valores del Esquema
 module.exports = mongoose.model("Usuarios", UsuariosSchema);
