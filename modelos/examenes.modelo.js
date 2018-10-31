cd1ecc3016e80c3c2e84bc7ec2ea82c6a6aad73c
Gustavo <gcallejas@grupoeducare.com>
2018-10-31 17:56:59 -0600

    Actualizacion

--- modelos/examenes.modelo.js
+++ modelos/examenes.modelo.js
@@ -6,7 +6,12 @@ var Schema = mongoose.Schema;
 
 var SlidesExamenes = Schema({
 	nombre: { type: String, required: [true, 'El	nombre	es	necesario'] },
-	imagen: { type: String, required: false },
+	imagen: { type: String, required: false, default:''},
+	
+	licencia: { type: String, required: false , default:''},
+	desbloquear: { type: Boolean, required: false , default:false},
+	resetear: { type: Boolean, required: false , default:false},
+	
 	usuario: { type: Schema.Types.ObjectId, ref: 'Usuarios', required: true },
 	grupo: {type: Schema.Types.ObjectId, ref: 'Grupos', required: [true, 'El	id	Grupos es	un	campo	obligatorio']	}
 })
